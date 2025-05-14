31f70f3145330b028de7e3f54d6013d7beffc252
Renata Costa Rocha <recr17@hotmail.com>
2025-05-14 20:23:56 -0300

    Atualiza Main para executar comandos do interpretador

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
-// Main.java
 public class Main {
     public static void main(String[] args) {
-        String input = """
-            let a = 42 + 5 - 8;
-            let b = 56 + 8;
-            print a + b + 6;
+
+        String codigo = """
+            PUSH 10
+            PUSH 5
+            ADD
+            POP x
+            PUSH x
+            PUSH 2
+            SUB
+            PRINT
         """;
 
-        Parser parser = new Parser(input);
-        parser.parse();
+        Interpretador interpretador = new Interpretador(codigo);
+        interpretador.run();
     }
 }
